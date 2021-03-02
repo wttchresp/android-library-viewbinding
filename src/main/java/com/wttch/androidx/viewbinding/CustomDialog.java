ab9e7099550f9bd11752b20049dfc853acf3832d
wttch <wttch@wttch.com>
2021-03-02 20:24:05 +0800

    feature: [viewbinding] 通用的对话框

--- src/main/java/com/wttch/androidx/viewbinding/CustomDialog.java
+++ src/main/java/com/wttch/androidx/viewbinding/CustomDialog.java
@@ -2,15 +2,29 @@ package com.wttch.androidx.viewbinding;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import androidx.annotation.DimenRes;
 import androidx.viewbinding.ViewBinding;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * 通用的 ViewBinding 绑定的对话框.
+ *
+ * @param <Binding> 绑定的 ViewBinding 类型
+ */
 public class CustomDialog<Binding extends ViewBinding> extends ViewBindingDialog<Binding> {
 
-
-  public CustomDialog(Builder<Binding> builder) {
-    super(builder.context, builder.cancelable, builder.cancelListener);
+  /**
+   * 使用构建器构建通用的对话框
+   *
+   * @param builder 构建器对象
+   */
+  private CustomDialog(Builder<Binding> builder) {
+    super(builder.context, R.style.CustomDialog);
+    setCancelable(builder.cancelable);
+    setOnCancelListener(builder.cancelListener);
+    width(builder.width);
+    height(builder.height);
     this.binding = builder.binding;
   }
 
@@ -20,6 +34,13 @@ public class CustomDialog<Binding extends ViewBinding> extends ViewBindingDialog
     return binding;
   }
 
+  /**
+   * CustomDialog 使用的构建器
+   *
+   * @param context   android 上下文
+   * @param binding   ViewBinding 对象
+   * @param <Binding> ViewBinding 类型
+   */
   public static <Binding extends ViewBinding> Builder<Binding> builder(Context context,
       Binding binding) {
     return new Builder<>(context, binding);
@@ -29,6 +50,8 @@ public class CustomDialog<Binding extends ViewBinding> extends ViewBindingDialog
 
     private final Context context;
     private final Binding binding;
+    private int width;
+    private int height;
     private boolean cancelable = false;
     @Nullable
     private DialogInterface.OnCancelListener cancelListener;
@@ -40,6 +63,16 @@ public class CustomDialog<Binding extends ViewBinding> extends ViewBindingDialog
 
     public CustomDialog<Binding> build() {
       return new CustomDialog<>(this);
+    }
+
+    public Builder<Binding> width(@DimenRes int width) {
+      this.width = width;
+      return this;
+    }
+
+    public Builder<Binding> height(@DimenRes int height) {
+      this.height = height;
+      return this;
     }
 
     public Builder<Binding> cancelable(boolean cancelable) {
--- src/main/java/com/wttch/androidx/viewbinding/ViewBindingDialog.kt
+++ src/main/java/com/wttch/androidx/viewbinding/ViewBindingDialog.kt
@@ -1,15 +1,21 @@
 package com.wttch.androidx.viewbinding
 
+
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
+import androidx.annotation.DimenRes
+import androidx.annotation.StyleRes
 import androidx.viewbinding.ViewBinding
 import com.wttch.android.util.SizeUtils
 
-abstract class ViewBindingDialog<Binding : ViewBinding>(context: Context, cancelable: Boolean, cancelListener: DialogInterface.OnCancelListener?)
-  : Dialog(context, cancelable, cancelListener), ViewBindable<Binding> {
+/**
+ * ViewBinding 方式的 Dialog.
+ */
+abstract class ViewBindingDialog<Binding : ViewBinding>(context: Context, @StyleRes styleId: Int) : Dialog(context, styleId), ViewBindable<Binding> {
   protected lateinit var binding: Binding
+  private var width = 200
+  private var height = 300
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -17,12 +23,48 @@ abstract class ViewBindingDialog<Binding : ViewBinding>(context: Context, cancel
     setContentView(binding.root)
 
     setCanceledOnTouchOutside(true)
-
     window?.let {
       val lp = it.attributes
-      lp.height = SizeUtils.dp2px(200f)
-      lp.width = SizeUtils.dp2px(300f)
+      lp.height = height
+      lp.width = width
       it.attributes = lp
     }
   }
+
+  /**
+   * 设置 Dialog 的宽度资源 [res].
+   * 链式调用, 同时返回自身.
+   */
+  fun width(@DimenRes res: Int): ViewBindingDialog<Binding> = apply { this.width = context.resources.getDimension(res).toInt() }
+
+  /**
+   * 设置 Dialog 的宽度 [pxValue], 单位 px.
+   * 链式调用, 同时返回自身.
+   */
+  fun pxWidth(pxValue: Float): ViewBindingDialog<Binding> = apply { this.width = pxValue.toInt() }
+
+  /**
+   * 设置 Dialog 的宽度 [dpValue], 单位 dp.
+   * 链式调用, 同时返回自身.
+   */
+  fun dpWidth(dpValue: Float): ViewBindingDialog<Binding> = apply { this.width = SizeUtils.dp2px(dpValue) }
+
+  /**
+   * 设置 Dialog 的高度资源 [res].
+   * 链式调用, 同时返回自身.
+   */
+  fun height(@DimenRes res: Int): ViewBindingDialog<Binding> = apply { this.height = context.resources.getDimension(res).toInt() }
+
+  /**
+   * 设置 Dialog 的高度 [pxValue], 单位 px.
+   * 链式调用, 同时返回自身.
+   */
+  fun pxHeight(pxValue: Float): ViewBindingDialog<Binding> = apply { this.height = pxValue.toInt() }
+
+  /**
+   * 设置 Dialog 的高度 [dpValue], 单位 dp.
+   * 链式调用, 同时返回自身.
+   */
+
+  fun dpHeight(dpValue: Float): ViewBindingDialog<Binding> = apply { this.width = SizeUtils.dp2px(dpValue) }
 }
